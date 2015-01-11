60707c00a74e6efe13af7cacaaf8d41d78ca6e92
elmagnif <msptechno12>
2015-01-11 22:15:24 +0000

    Initial commit

---
+++ app/src/main/java/info/androidhive/customlistviewvolley/ArticleRead.java
@@ -0,0 +1,15 @@
+package info.androidhive.customlistviewvolley;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by apple on 06/01/15.
+ */
+public class ArticleRead extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_read);
+    }
+}
